f70b8da488d51a6a6081c6dbde74c29c0f5b8790
Syed Saif Raza <Codewithsaif@gmail.com>
2022-05-06 15:35:58 +0530

    Git First Comimt

---
+++ lib/login.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class MyLogin extends StatefulWidget {
+  const MyLogin({Key? key}) : super(key: key);
+
+  @override
+  State<MyLogin> createState() => _MyLoginState();
+}
+
+class _MyLoginState extends State<MyLogin> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          image: (DecorationImage(
+              image: AssetImage('login-screen.png'), fit: BoxFit.cover))),
+    );
+  }
+}
